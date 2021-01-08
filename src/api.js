7c27b86625d880fbb3cab9eea87b037d1dce7375
andreamazzatxt <73519803+andreamazzatxt@users.noreply.github.com>
2021-01-08 17:17:04 +0100

    Change color of ranking window

--- src/api.js
+++ src/api.js
@@ -66,3 +66,31 @@ export async function tvWeekly(){
     return result;
 
 }
+
+export async function upcoming(){
+    let uri = "https://api.themoviedb.org/3/movie/upcoming?api_key=84f85a58146a2d2a9feeea9c4cf55735&language=en-US&page=1";
+    let response = await fetch(uri);
+        response = await response.json();
+    let result = await response.results.map(element =>{
+        return {
+            id: element.id,
+            title: element.title,
+            resume: element.overview,
+            avatarImg : element.poster_path ?  imgUrl+element.poster_path : '',
+            backImg: element.backdrop_path ?  imgUrl+element.backdrop_path : '',
+            date: element.release_date,
+            vote: element.vote_avarage
+        }
+    })
+    result = result.filter(element =>{
+        console.log(element.date)
+       let release = new Date(element.date).getTime();
+       let today = new Date().getTime();
+       console.log(release);
+       console.log(release >= today)
+       return release >= today || !release;
+    })
+    return result;
+
+}
+
--- src/components/Window.js
+++ src/components/Window.js
@@ -1,5 +1,5 @@
 import style from './Window.module.css'
-import {tvWeekly, weekly} from '../api';
+import {tvWeekly, upcoming, weekly} from '../api';
 import { useEffect, useState } from 'react';
 import Tile from './Tile';
 import { motion } from "framer-motion";
@@ -26,6 +26,7 @@ const container = {
 function Window(props) {
     const [week,setWeek] = useState([])
     const [tvWeek,setTvWeek] = useState([])
+    const [upcomingList,setUpcomingList] = useState([]);
     useEffect(()=>{
         async function fetch(){
             let resp = await  weekly();
@@ -40,6 +41,13 @@ function Window(props) {
       }
       fetch();
   },[])
+  useEffect(()=>{
+    async function fetch(){
+        let resp = await  upcoming();
+   setUpcomingList(resp); 
+    }
+    fetch();
+},[])
     
     return(
         <div className={style.window}>
@@ -50,17 +58,42 @@ function Window(props) {
             animate="visible"
             id={style.rated} className = {style.carrousel}>
                 {
-                    week.map(element =>{
+                    week.map((element,index) =>{
                         return(
                             <motion.div
                             key={element.id+"tile"}
-                            variants={item}>
-                                <Tile data = {element}/>
+                            variants={item}
+                            className={style.tileContainer}
+                            >
+                            {   index < 9                           
+                              && <div className={style.rank}>{index+1}</div>
+                            }                                  <Tile data = {element}/>
                             </motion.div>
                             )    
                     })
                 }
             </motion.div>
+              <span className={style.title}>Upcoming Movies</span>
+                        <motion.div 
+                        variants={container}
+                        initial="hidden"
+                        animate="visible"
+                        id={style.rated} className = {style.carrousel}>
+                            {
+                                upcomingList.map((element,index) =>{
+                                    return(
+                                        <motion.div
+                                        key={element.id+"tile"}
+                                        variants={item}
+                                        className={style.tileContainer}>
+                                                                      
+                                        <Tile data = {element}/>
+                                        </motion.div>
+                                        )    
+                                })
+                            }
+                        </motion.div>
+
             <span className={style.title}>Weekly trending Series</span>
             <motion.div 
             variants={container}
@@ -68,17 +101,22 @@ function Window(props) {
             animate="visible"
             id={style.rated} className = {style.carrousel}>
                 {
-                    tvWeek.map(element =>{
+                    tvWeek.map((element,index) =>{
                         return(
                             <motion.div
                             key={element.id+"tile"}
-                            variants={item}>
-                                <Tile data = {element}/>
+                            variants={item}
+                            className={style.tileContainer}>
+                            {   index < 9                           
+                              && <div className={style.rank}>{index+1}</div>
+                            }                                
+                            <Tile data = {element}/>
                             </motion.div>
                             )    
                     })
                 }
             </motion.div>
+          
         </div>
     )
 }
